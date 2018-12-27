a834734236a6e70f0ba66a0f1c65be1866b70c82
fernando.h.alves <fernando.h.alves@kroton.com.br>
2018-12-27 10:31:35 -0200

    Adiciona log em calculos fatura

--- src/main/java/br/com/gestaogastos/cartao/service/FaturaService.java
+++ src/main/java/br/com/gestaogastos/cartao/service/FaturaService.java
@@ -3,30 +3,40 @@ package br.com.gestaogastos.cartao.service;
 import br.com.gestaogastos.cartao.model.Cartao;
 import br.com.gestaogastos.cartao.model.FaturaLancamento;
 import org.javamoney.moneta.Money;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import javax.money.MonetaryAmount;
 import java.util.List;
 
 @Service
 public class FaturaService {
 
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
     public Money calculaTotalFatura(List<FaturaLancamento> faturaLancamentos) {
-
         Money total = Money.of(0, "BRL");
-
-        for (FaturaLancamento fatura : faturaLancamentos) {
-            total = total.add(Money.of(fatura.getValor().getNumber(), "BRL"));
+        try {
+            for (FaturaLancamento fatura : faturaLancamentos) {
+                total = total.add(Money.of(fatura.getValor().getNumber(), "BRL"));
+            }
+        } catch (Exception e) {
+            log.error("Erro calculaTotalFatura ", e);
         }
-
         return total;
     }
 
     public Money calculaSaldoLimiteFatura(List<FaturaLancamento> faturaLancamentos) {
-        Cartao cartao = faturaLancamentos.get(0).getFatura().getCartao();
+        Money saldo = Money.of(0, "BRL");
+        try {
+            Cartao cartao = faturaLancamentos.get(0).getFatura().getCartao();
 
-        Money total = calculaTotalFatura(faturaLancamentos);
+            Money total = calculaTotalFatura(faturaLancamentos);
 
-        return cartao.getLimite().subtract(total);
+            saldo = cartao.getLimite().subtract(total);
+        } catch (Exception e) {
+            log.error("Erro calculaSaldoLimiteFatura ", e);
+        }
+        return  saldo;
     }
 }
--- src/test/java/br/com/gestaogastos/fatura/FaturaServiceTest.java
+++ src/test/java/br/com/gestaogastos/fatura/FaturaServiceTest.java
@@ -11,12 +11,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.money.CurrencyUnit;
-import javax.money.Monetary;
 import java.util.Arrays;
 import java.util.List;
 
