bd530e7807cf6b4e3379db4963eb99278f81a07d
fernando.h.alves <fernando.h.alves@kroton.com.br>
2019-01-03 15:17:10 -0200

    Adiciona join consulta cartões e comenta logs trace em console

--- src/main/java/br/com/gestaogastos/cartao/repository/CartaoRepositoryImpl.java
+++ src/main/java/br/com/gestaogastos/cartao/repository/CartaoRepositoryImpl.java
@@ -1,7 +1,8 @@
 package br.com.gestaogastos.cartao.repository;
 
 import br.com.gestaogastos.cartao.dto.CartaoDto;
-import br.com.gestaogastos.cartao.model.QCartao;
+import static br.com.gestaogastos.cartao.model.QBanco.banco;
+import static br.com.gestaogastos.cartao.model.QCartao.cartao;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -12,16 +13,14 @@ import java.util.List;
 
 public class CartaoRepositoryImpl implements CartaoRepositoryCustom {
 
-    public static final QCartao cartao = new QCartao("cartao");
-
     @Autowired
     EntityManager entityManager;
 
     public List<CartaoDto> getAll(Predicate predicate) {
-        QCartao cartao = new QCartao("cartao");
         return new JPAQueryFactory(entityManager)
                 .select(Projections.constructor(CartaoDto.class, cartao.id, cartao.nome, cartao.banco.id))
                 .from(cartao)
+                .join(cartao.banco, banco)
                 .where(predicate)
                 .fetch();
     }
