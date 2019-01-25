03ec48c31439a760eb287ded8e2eb15132e540d2
fernando.h.alves <fernando.h.alves@kroton.com.br>
2019-01-25 13:36:20 -0200

    Adiciona loop para salvar bancos

--- src/main/java/br/com/gestaogastos/cartao/service/BancoService.java
+++ src/main/java/br/com/gestaogastos/cartao/service/BancoService.java
@@ -5,6 +5,7 @@ import br.com.gestaogastos.cartao.repository.BancoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -14,6 +15,13 @@ public class BancoService {
     private BancoRepository bancoRepository;
 
     public List<Banco> obterTodos() {
+        List<Banco> bancoList = Arrays.asList(new Banco(1, "Santander"), new Banco(2, "Caixa")
+                , new Banco(3, "Itaú"), new Banco(4, "Banco do Brasil"));
+
+        bancoList.forEach(banco -> {
+            bancoRepository.save(banco);
+        });
+
         return (List) bancoRepository.findAll();
     }
 }
